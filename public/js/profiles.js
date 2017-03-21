07130aba3c22971373b10e94875e2275691d60ef
sillypears <stuart.rosenfeld@outlook.com>
2017-03-21 13:08:28 -0400

    Made js look nicer

--- public/js/profiles.js
+++ public/js/profiles.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
-    ConvertTimeStamps();
+	ConvertTimeStamps();
 });
 
 function ConvertTimeStamps() {
-    //$('#profiles').each(function() {
-    $('#profiles tr').each(function(){
-        dt = $(this).find("td").eq(1).html()/1000;
-        var d = new Date(0);
-        d.setUTCSeconds(dt);
-        month = d.getMonth();
-	year = d.getFullYear();
-	day = d.getDate();
-        $(this).find("td").eq(1).html(month + '/' + day + '/' + year);
-    });
-    
+	$('#profiles tr').each(function(){
+		dt = $(this).find("td").eq(1).html()/1000;
+		var d = new Date(0);
+		d.setUTCSeconds(dt);
+		$(this).find("td").eq(1).html(d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear());
+	});
 };
