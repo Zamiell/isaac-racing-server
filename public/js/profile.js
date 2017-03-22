1c391fd2feacb5dd5d51acde2bc3a9d90d9facb5
sillypears <stuart.rosenfeld@outlook.com>
2017-03-21 23:47:38 -0400

    Fixed the month being off by 1 and a weird visual line under the twitch logo

--- public/js/profile.js
+++ public/js/profile.js
@@ -6,5 +6,5 @@ function ConvertTimeStamps() {
   dt = $('span#date').html()/1000;
   var d = new Date(0);
   d.setUTCSeconds(dt);
-  $('span#date').html(d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear());
+  $('span#date').html((d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear());
 };
