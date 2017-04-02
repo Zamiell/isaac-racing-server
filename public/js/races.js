80d92b52b39b203ef4586503d245f252197208f7
sillypears <stuart.rosenfeld@outlook.com>
2017-04-02 08:01:52 -0400

    One more push :D

--- public/js/races.js
+++ public/js/races.js
@@ -5,8 +5,10 @@ $(document).ready(function () {
 function ConvertTimeStamps() {
 	$('#race-listing-table tr').each(function(){
 		if ($(this).find('td').is('#racedate')) {
-			dt = $(this).find("#racedate").eq(0).html()/1000;
+			dt = Math.floor($(this).find("#racedate").eq(0).html()/1000);
+			console.log(dt);
 			var d = new Date(0);
+			d.setUTCSeconds(dt);
 			$(this).find("td#racedate").html((d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes	());
 		}
 	});
