112d8dd8e1e124efd8c8e1c3d380301067999d43
sillypears <stuart.rosenfeld@outlook.com>
2017-04-02 08:18:21 -0400

    Few more QOL changes

--- public/js/races.js
+++ public/js/races.js
@@ -6,10 +6,9 @@ function ConvertTimeStamps() {
 	$('#race-listing-table tr').each(function(){
 		if ($(this).find('td').is('#racedate')) {
 			dt = Math.floor($(this).find("#racedate").eq(0).html()/1000);
-			console.log(dt);
 			var d = new Date(0);
 			d.setUTCSeconds(dt);
-			$(this).find("td#racedate").html((d.getMonth()+1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + ("0"+d.getMinutes()).slice(-2));
+			$(this).find("td#racedate").html((d.getUTCMonth()+1) + '/' + d.getUTCDate() + '/' + d.getUTCFullYear() + ' ' + d.getUTCHours() + ':' + ("0"+d.getUTCMinutes()).slice(-2) + ' UTC');
 		}
 	});
 };
