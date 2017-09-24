ef13b893a3cc5c71a17a33251f45b348ffa89da5
James Nesta <j.nesta@gmail.com>
2017-09-24 23:22:53 +0000

    download script needs to change since installer new filename

--- public/js/download.js
+++ public/js/download.js
@@ -5,8 +5,12 @@ $(document).ready(function () {
 function GetLatestReleaseInfo() {
     $.getJSON('https://api.github.com/repos/Zamiell/isaac-racing-client/releases/latest').done(function(json) {
         // Make the Windows download button
-        // (the 0th element of the array is always the "latest.yml" file)
-        var downloadURL = json.assets[1].browser_download_url;
+        var downloadURL = json.assets[3].browser_download_url;
+        // The elements of the array correspond to the order that the files are uploaded
+        // 0 - latest.yml
+        // 1 - RacingPlus-#.##.##-ia32.nsis.7z
+        // 2 - RacingPlus-#.##.##-x64.nsis.7z
+        // 3 - RacingPlus-WebSetup-#.##.##.exe
         $('#download-button-windows').attr('href', downloadURL);
 
         // Make the OS X download button
