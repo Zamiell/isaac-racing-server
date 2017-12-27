a1b0d31ef08dbd095a78e5275162f368a1495646
James Nesta <j.nesta@gmail.com>
2017-12-27 21:30:54 +0000

    some comments for later

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GOPATH=/root/go /usr/local/go/bin/go run /root/go/src/github.com/Zamiell/isaac-racing-server/*.go
+GOPATH=/root/go /usr/local/go/bin/go run /root/go/src/github.com/Zamiell/isaac-racing-server/src/*.go
