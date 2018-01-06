7a7bf1d10b0b287287299548fd543dda4ed75cb1
sillypears <sillypears@users.noreply.github.com>
2018-01-05 21:15:39 -0500

    Added a "common" function file for JS Need to clean up all JS functions and move more to this

--- public/js/profile.js
+++ public/js/profile.js
@@ -15,12 +15,6 @@ $(document).ready(function() {
     });
 });
 
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-};
-
 function ConvertRaceTime(td) {
   $(td).each(function(){
       runtime = Math.floor($(this).html()/ 1000);
