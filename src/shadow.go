93ed3a1bcb3c6307bcea9129d4bfdfeb6af3e5ad
hennadii-demchenko <impskievua@gmail.com>
2020-05-31 17:50:22 +0300

    [WIP] Implement UDP shadow server module
    Signed-off-by: hennadii-demchenko <impskievua@gmail.com>

---
+++ src/shadow.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/Zamiell/isaac-racing-server/src/log"
+	"net"
+)
+
+const (
+	sessionTTL = 30
+)
+
+type PlayerMap map[uint32]map[uint32]*PlayerConn
+
+type PlayerConn struct {
+	CONN *net.UDPConn
+	TTL  uint
+}
+
+func (pc *PlayerConn) finalizeConnection(playerId uint32) {
+	if pc.CONN != nil {
+		err := pc.CONN.Close()
+		if err != nil {
+			log.Error(fmt.Sprintf("Error closing connection player=%v ", playerId), err)
+		} else {
+			log.Info(fmt.Sprintf("Connection closed for player=%v", playerId))
+		}
+	}
+}
+
+type MessageHeader struct {
+	RaceId   uint32
+	PlayerId uint32
+}
+
+func (m *MessageHeader) Unmarshall(b []byte) (err error) {
+	reader := bytes.NewReader(b)
+	err = binary.Read(reader, binary.LittleEndian, m)
+	if err != nil {
+		log.Error(fmt.Sprintf("Failed to unmarshall message: %v", b), err)
+	}
+	return
+}
+
+func (p PlayerMap) getConnection(mh MessageHeader) *net.UDPConn {
+	if p[mh.RaceId] != nil && p[mh.RaceId][mh.PlayerId] != nil {
+		return p[mh.RaceId][mh.PlayerId].CONN
+	}
+	return nil
+}
+
+func (p PlayerMap) getOpponent(mh MessageHeader) (pConn *PlayerConn) {
+	mux.Lock()
+	defer mux.Unlock()
+
+	race := p[mh.RaceId]
+	if race == nil {
+		return
+	}
+	for pID, conn := range race {
+		if pID != mh.PlayerId {
+			pConn = conn
+		}
+	}
+	return
+}
+
+func (p *PlayerMap) updateSessions() {
+	mux.Lock()
+	defer mux.Unlock()
+
+	for raceId, race := range *p {
+		for playerId, pConn := range race {
+			if pConn == nil {
+				continue
+			}
+
+			pConn.TTL--
+			log.Info(fmt.Sprintf("[--] player=%v ttl=%v", playerId, pConn.TTL))
+
+			if pConn.TTL <= 0 {
+				pConn.finalizeConnection(playerId)
+				delete(race, playerId)
+				log.Info(fmt.Sprintf("Removing player=%v from race=%v due to timeout", playerId, raceId))
+
+				if len(race) < 1 {
+					delete(*p, raceId)
+					log.Info(fmt.Sprintf("Record removed race=%v", raceId))
+				}
+			}
+		}
+	}
+}
+
+func (p *PlayerMap) update(mh MessageHeader, addr string) {
+	defer mux.Unlock()
+
+	raddr, _ := net.ResolveUDPAddr("udp", addr)
+
+	mux.Lock()
+
+	// lazy-init race
+	if (*p)[mh.RaceId] == nil {
+		(*p)[mh.RaceId] = make(map[uint32]*PlayerConn)
+		log.Info(fmt.Sprintf("Record created race=%v ", mh.RaceId))
+	}
+
+	race := (*p)[mh.RaceId]
+
+	// lazy-init player connection
+	if race[mh.PlayerId] == nil {
+		if len(race) > 1 {
+			log.Info(fmt.Sprintf("Player=%v attempted to join race=%v with two players", mh.PlayerId, mh.RaceId))
+			return
+		}
+		conn, _ := net.DialUDP("udp4", nil, raddr)
+		race[mh.PlayerId] = &PlayerConn{conn, sessionTTL}
+		log.Info(fmt.Sprintf("Connection created player=%v, dst=%v", mh.PlayerId, raddr))
+	}
+
+	// update TTL
+	race[mh.PlayerId].TTL = sessionTTL
+	log.Info(fmt.Sprintf("TTL updated race=%v, player=%v", mh.RaceId, mh.PlayerId))
+}
---
+++ src/shadowServer.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Zamiell/isaac-racing-server/src/log"
+	"net"
+	"sync"
+	"time"
+	"unsafe"
+)
+
+const (
+	maxBufferSize = 1024
+	helloSize     = 5
+	port          = 9001
+	clockInterval = 1
+)
+
+var (
+	mux  = sync.Mutex{}
+	pMap = make(PlayerMap)
+)
+
+func verifySender(mh MessageHeader, raddr net.Addr) bool {
+	playerConnection := pMap.getConnection(mh)
+	if playerConnection == nil {
+		log.Info(fmt.Sprintf("No player=%v session found, ignoring", mh.PlayerId))
+		return false
+	} else if playerConnection.RemoteAddr().String() != raddr.String() {
+		log.Info(fmt.Sprintf("Player=%v shadow comes has untracked origin, recorded=%v, received=%v",
+			mh.PlayerId, playerConnection.RemoteAddr().String(), raddr.String()))
+		return false
+	}
+	return true
+}
+
+func handleHelloMessage(msg []byte, addr net.Addr) {
+	log.Info("Parsing hello message")
+	mh := MessageHeader{}
+	err := mh.Unmarshall(msg)
+	if err == nil {
+		pMap.update(mh, addr.String())
+	}
+}
+
+func handleShadowMessage(msg []byte, raddr net.Addr) {
+	mh := MessageHeader{}
+	err := mh.Unmarshall(msg)
+
+	if err == nil {
+		log.Info(fmt.Sprintf("Shadow received, player=%v", mh.PlayerId))
+		if !verifySender(mh, raddr) {
+			return
+		}
+
+		opponent := pMap.getOpponent(mh)
+		if opponent != nil {
+			log.Info(fmt.Sprintf("Opponen found, player=%v", mh.PlayerId))
+			log.Info(fmt.Sprintf("Proxying shadow, [src=%v]=>[dst=%v]", raddr, opponent.CONN.RemoteAddr()))
+			_, err := opponent.CONN.Write(msg)
+			if err != nil {
+				log.Debug(fmt.Sprintf(
+					"Shadow proxy failed, player=%v, msg: %v\ncause: %v", mh.PlayerId, msg, err))
+			}
+		} else {
+			log.Info(fmt.Sprintf("Missing opponent session, player=%v", mh.PlayerId))
+		}
+	}
+}
+
+func shadowServer(address string) (err error, pc net.PacketConn) {
+	pc, err = net.ListenPacket("udp4", address)
+	log.Info("Listening UDP connections on", address)
+	if err != nil {
+		log.Error("Error starting UDP shadow server", err)
+		return
+	}
+
+	buffer := make([]byte, maxBufferSize)
+
+	go func() {
+		for {
+			n, raddr, err := pc.ReadFrom(buffer)
+			if err != nil {
+				log.Error(fmt.Sprintf("Error receiving UDP dgram from %v", raddr.String()), err)
+			}
+			if n > 0 {
+				log.Info(fmt.Sprintf("Received dgram on shadow service: bytes=%d src=%s\n", n, raddr.String()))
+			}
+			payloadSize := n - int(unsafe.Sizeof(MessageHeader{}))
+			if payloadSize < helloSize {
+				log.Info("Dgram skipped, payload len=", payloadSize)
+				// skip
+			} else if payloadSize == helloSize {
+				handleHelloMessage(buffer, raddr)
+			} else {
+				handleShadowMessage(buffer, raddr)
+			}
+		}
+	}()
+
+	return
+}
+
+func sessionClock() {
+	tick := time.Tick(clockInterval * time.Second)
+	for {
+		select {
+		case <-tick:
+			pMap.updateSessions()
+		default:
+			time.Sleep(clockInterval * time.Second)
+		}
+	}
+}
+
+func shadowInit() {
+	go sessionClock()
+	errStart, pc := shadowServer(fmt.Sprintf("127.0.0.1:%d", port))
+	if errStart != nil {
+		log.Error("Exited by: ", errStart)
+		if pc != nil {
+			errClose := pc.Close()
+			if errClose != nil {
+				log.Error("Error closing connection", errClose)
+			}
+		}
+	}
+}
