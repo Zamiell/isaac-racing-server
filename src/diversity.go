555c55f8701f848bad9172829d8157ab37efb288
Rchardon <33094562+Rchardon@users.noreply.github.com>
2021-11-28 23:11:06 +0100

    remove item 733 from div

--- src/diversity.go
+++ src/diversity.go
@@ -113,9 +113,6 @@ var validDiversityPassiveItems = [...]int{
 	681, 682, 683, 684, 686, 688, 689, 690, 691, 692,
 	693, 694, 695, 696, 697, 698, 699, 700, 701, 702, // Supper (707) is banned
 	708, 716, 717, 724, 725, 726, 727, 730, 731, 732,
-
-	// Custom items
-	733,
 }
 
 var validDiversityTrinkets = [...]int{
