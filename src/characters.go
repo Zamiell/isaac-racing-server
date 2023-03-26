b9d768cd3d84b30567d873f4cc177d9331122d6e
James <5511220+Zamiell@users.noreply.github.com>
2023-03-26 18:45:19 -0400

    chore: char documentation

--- src/characters.go
+++ src/characters.go
@@ -2,40 +2,52 @@ package server
 
 var (
 	characters = []string{
-		"Isaac",             // 0
-		"Magdalene",         // 1
-		"Cain",              // 2
-		"Judas",             // 3
-		"Blue Baby",         // 4
-		"Eve",               // 5
-		"Samson",            // 6
-		"Azazel",            // 7
-		"Lazarus",           // 8
-		"Eden",              // 9
-		"The Lost",          // 10
-		"Lilith",            // 11
-		"Keeper",            // 12
-		"Apollyon",          // 13
-		"The Forgotten",     // 14
-		"Bethany",           // 15
-		"Jacob & Esau",      // 16
-		"Tainted Isaac",     // 17
-		"Tainted Magdalene", // 18
-		"Tainted Cain",      // 19
-		"Tainted Judas",     // 20
-		"Tainted Blue Baby", // 21
-		"Tainted Eve",       // 22
-		"Tainted Samson",    // 23
-		"Tainted Azazel",    // 24
-		"Tainted Lazarus",   // 25
-		"Tainted Eden",      // 26
-		"Tainted Lost",      // 27
-		"Tainted Lilith",    // 28
-		"Tainted Keeper",    // 29
-		"Tainted Apollyon",  // 30
-		"Tainted Forgotten", // 31
-		"Tainted Bethany",   // 32
-		"Tainted Jacob",     // 33
-		"Random Baby",       // 34
+		// Normal characters
+		"Isaac",     // 0
+		"Magdalene", // 1
+		"Cain",      // 2
+		"Judas",     // 3
+		"Blue Baby", // 4
+		"Eve",       // 5
+		"Samson",    // 6
+		"Azazel",    // 7
+		"Lazarus",   // 8
+		"Eden",      // 9
+		"The Lost",  // 10
+		// "Lazarus II", // 11
+		// "Dark Judas", // 12
+		"Lilith",        // 13
+		"Keeper",        // 14
+		"Apollyon",      // 15
+		"The Forgotten", // 16
+		// "The Soul", // 17
+		"Bethany",      // 18
+		"Jacob & Esau", // 19
+		// "Esau", // 20
+
+		// Tainted characters
+		"Tainted Isaac",     // 21
+		"Tainted Magdalene", // 22
+		"Tainted Cain",      // 23
+		"Tainted Judas",     // 24
+		"Tainted Blue Baby", // 25
+		"Tainted Eve",       // 26
+		"Tainted Samson",    // 27
+		"Tainted Azazel",    // 28
+		"Tainted Lazarus",   // 29
+		"Tainted Eden",      // 30
+		"Tainted Lost",      // 31
+		"Tainted Lilith",    // 32
+		"Tainted Keeper",    // 33
+		"Tainted Apollyon",  // 34
+		"Tainted Forgotten", // 35
+		"Tainted Bethany",   // 36
+		"Tainted Jacob",     // 37
+		// "Dead Tainted Lazarus", // 38
+		// "Dead Tainted Jacob", // 39
+		// "Tainted Soul", // 40
+
+		// Other
+		"Random Baby",
 	}
 )
